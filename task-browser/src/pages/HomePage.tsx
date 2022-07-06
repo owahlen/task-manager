0295a50ab9b72ab743a3d1867e541ce4222bdf0a
Oliver Wahlen <oliver.wahlen@infinit-group.de>
2022-07-06 22:46:15 +0200

    update of react

---
+++ task-browser/src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const HomePage = () => {
+
+    return (
+        <div>
+            <h1 className="text-green-800 text-4xl">Welcome to the Homepage</h1>
+        </div>
+    );
+};
+
+export default HomePage;
---
+++ task-browser/src/pages/SecuredPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const SecuredPage = () => {
+
+    return (
+        <div>
+            <h1 className="text-black text-4xl">Welcome to the Protected Page.</h1>
+        </div>
+    );
+};
+
+export default SecuredPage;
